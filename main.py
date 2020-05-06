d73fb44841ec100b75cf48b98af53971a7995c42
Gooosee <46865601+Gooosee@users.noreply.github.com>
2020-05-06 22:55:46 +0300

    Кнопки входа и регистрации

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import flask
-
+from flask import redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 app = flask.Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 @app.route('/')
@@ -22,6 +34,14 @@ def leaderboard():
 @app.route('/download')
 def download():
     return flask.render_template('download.html')
+
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return flask.render_template('registr.html', title='Авторизация', form=form)
 
 
 @app.route('/donate')
