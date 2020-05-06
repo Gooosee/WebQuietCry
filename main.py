f6c83cf74a6d0bc1dca63bad3b06cff512c44db4
Vlad <saromanovlad@gmail.com>
2020-05-06 16:20:04 +0400

    Работа вкладок

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ app = flask.Flask(__name__)
 @app.route('/')
 @app.route('/news')
 def main():
-    return flask.render_template('WebQuietCry.html')
+    return flask.render_template('news.html')
 
 
 @app.route('/about_us')
 def about_us():
-    return flask.render_template('WebQuietCry.html')
+    return flask.render_template('about_us.html')
 
 
 @app.route('/leaderboard')
 def leaderboard():
-    return flask.render_template('WebQuietCry.html')
+    return flask.render_template('leaderboard.html')
 
 
 @app.route('/download')
 def download():
-    return flask.render_template('WebQuietCry.html')
+    return flask.render_template('download.html')
 
 
 @app.route('/donate')
 def donate():
-    return flask.render_template('WebQuietCry.html')
+    return flask.render_template('donate.html')
 
 
 if __name__ == '__main__':
