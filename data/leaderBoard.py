fd073a8b396c7cc160cd2dca89548ed990f7e947
Gooosee <46865601+Gooosee@users.noreply.github.com>
2020-05-12 02:23:09 +0300

    Добавление в таблицу рекордов без шифра по адресу /addRecord/{здесь рекорд} Добавление, изменение, удаление новостей зарегистрированными пользователями.

---
+++ data/leaderBoard.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class LeaderBoard(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'LeaderBoard'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    record = sqlalchemy.Column(sqlalchemy.INTEGER, nullable=True)
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import flask
-from flask import redirect, render_template
-from flask_login import LoginManager, login_user, logout_user, login_required
+from flask import redirect, render_template, request
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from werkzeug.exceptions import abort
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 
 from data import db_session
+from data.leaderBoard import LeaderBoard
+from data.news import News
 from data.users import User
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+class NewsForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField("Содержание")
+    submit = SubmitField('Применить')
 
 
 class RegForm(FlaskForm):
@@ -28,6 +37,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class addRecordForm(FlaskForm):
+    login = StringField('Логин', validators=[DataRequired()])
+    submit = SubmitField('Добавить')
 
 
 @app.route('/logout')
@@ -46,7 +60,9 @@ def load_user(user_id):
 @app.route('/')
 @app.route('/news')
 def main():
-    return flask.render_template('news.html')
+    session = db_session.create_session()
+    news = session.query(News)
+    return render_template("news.html", news=news)
 
 
 @app.route('/about_us')
@@ -65,23 +81,26 @@ def download():
 
 
 @app.route('/reg', methods=['GET', 'POST'])
-def reg():
-    formR = RegForm()
-    if formR.validate_on_submit():
-        if formR.password.data == formR.password2.data:
-            userNew = User()
-            userNew.name = formR.login.data
-            userNew.email = formR.email.data
-            userNew.set_password(formR.password.data)
-            session = db_session.create_session()
-            session.add(userNew)
-            session.commit()
-            return redirect('/')
-        else:
-            return render_template('registr.html',
-                                   message="Повторите пароль",
-                                   form=formR)
-    return render_template('registr.html', title='Регистрация', form=formR)
+def reqister():
+    form = RegForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password2.data:
+            return render_template('registr.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('registr.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User()
+        user.name = form.login.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('registr.html', title='Регистрация', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -102,9 +121,82 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/addNews', methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        news = News()
+        news.title = form.title.data
+        news.content = form.content.data
+        current_user.news.append(news)
+        session.merge(current_user)
+        session.commit()
+        return redirect('/')
+    return render_template('addNews.html', title='Добавление новости',
+                           form=form)
+
+
+@app.route('/addNews/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = NewsForm()
+    if request.method == "GET":
+        session = db_session.create_session()
+        news = session.query(News).filter(News.id == id,
+                                          News.user == current_user).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        news = session.query(News).filter(News.id == id,
+                                          News.user == current_user).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            session.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('addNews.html', title='Редактирование новости', form=form)
+
+
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    session = db_session.create_session()
+    news = session.query(News).filter(News.id == id,
+                                      News.user == current_user).first()
+    if news:
+        session.delete(news)
+        session.commit()
+    else:
+        abort(404)
+    return redirect('/')
+
+
 @app.route('/donate')
 def donate():
     return flask.render_template('donate.html')
+
+
+@app.route('/addRecord/<int:score>', methods=['GET', 'POST'])
+def addRecord(score):
+    form = addRecordForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        LB = LeaderBoard()
+        LB.name = form.login.data
+        LB.record = int(score)
+        session.add(LB)
+        session.commit()
+        return redirect('/')
+    return render_template('addRecord.html', title='Добавление новости',
+                           form=form)
 
 
 if __name__ == '__main__':
