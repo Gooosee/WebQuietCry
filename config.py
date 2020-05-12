b1de6a1c7030b767eab98a106c743466029514ab
Gooosee <46865601+Gooosee@users.noreply.github.com>
2020-05-12 15:15:56 +0300

    config

---
+++ config.py
@@ -0,0 +1,7 @@
+POSTGRES = "postgres://squjcergemukcq:5a67f4a028ff7ceadf351e26322380546ffd979bb70c8e8a1091c2707b49d14c@ec2-54-228-250" \
+           "-82.eu-west-1.compute.amazonaws.com:5432/d7rs9bn804cvp2 "
+
+DB_FILE = "/db/comments.sqlite"  # если хотим оставить возможность переключиться обратно на sqlite
+SQLITE = f'sqlite://{DB_FILE}?check_same_thread=False'
+
+LOCAL_DB = POSTGRES
